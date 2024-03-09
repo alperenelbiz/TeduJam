cd5743110a16b3ace0333d86c1572633c03e5da9
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2024-03-09 14:23:01 +0300

    playerUI started

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+     int ammo;
+     int health;
+
+    [SerializeField] TextMeshPro ammoText;
+    [SerializeField] TextMeshPro healthText;
+
+
+    private void Awake()
     {
-        
+        ammo = GetComponent<Gun>().currentAmmo;
+        health = GetComponent<PlayerHealth>().currentHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ammoText.SetText(ammo.ToString());
+        healthText.SetText(health.ToString());
     }
 }
