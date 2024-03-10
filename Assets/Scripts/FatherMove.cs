acdf31ba7ac30718a4a2e5cfb9b8f757e20e5f24
barkinsarikartal <barkinsarikartal51@gmail.com>
2024-03-10 17:36:47 +0300

    vvf

---
+++ Assets/Scripts/FatherMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FatherMove : MonoBehaviour
+{
+    MoveToScreen moveScript;
+    public float delayTime;
+    public GameObject nesne1;
+    public GameObject nesne2;
+    public GameObject ui;
+
+    private void Start()
+    {
+        StartCoroutine(Movement());
+    }
+    private void Update()
+    {
+        
+
+    }
+
+    IEnumerator Movement()
+    {
+        ui.SetActive(false);
+        Debug.Log("basladi");
+        moveScript = GameObject.Find("UIController").GetComponent<MoveToScreen>();
+        while(true) 
+        {
+            if (moveScript.HedefeHareketEt(nesne1))
+            {
+                break;
+            }
+            yield return null;
+        }
+        yield return new WaitForSeconds(delayTime);
+        while (true)
+        {
+            if (moveScript.HedefeHareketEt(nesne2))
+            {
+                break;
+            }
+            yield return null;
+        }
+        ui.SetActive(true);
+        Debug.Log("bitti");
+    }
+}
